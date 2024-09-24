539eac1b27e725dcea0851991c0a3268aa8e074c
Francisco Ferreira <ferreiradeveloper@gmail.com>
2024-09-25 00:05:18 +0200

    dos promeros ejemplos

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Franco',
+    age: 55,
+    nacionality: 'Venezuela',
+    profession: 'SQL Developer',
+    interets: ['SQL', 'DATA', 'Database','Sport']
+}
+
+console.log(person);
+
+const {name, age} = person;
+console.log(name);
+console.log(age);
---
+++ tuplas_dinamicas.py
@@ -0,0 +1,19 @@
+
+# tupla
+tupla = tuple(
+    i 
+    for i in range(10)
+    if i % 2 == 0
+    )
+print(tupla)
+
+
+# lista
+lista = list(
+    i 
+    for i in range(10)
+    if i % 2 == 0
+    )
+print(lista)
+
+
